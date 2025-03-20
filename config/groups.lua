c06554680e1cd296d03d876a23260ddcfb132ef7
Madruga_Dev <madrugadev01@gmail.com>
2025-03-20 18:30:20 -0300

    Initial commit

---
+++ config/groups.lua
@@ -0,0 +1,22 @@
+Actions = {
+    all = {
+        types = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 },
+        desc = "Todos os itens",
+        img = "satchel_nav_all.png"
+    },
+    medical = { 
+        types = { 0, 2 }, 
+        desc = "Armazena todos os tipos de itens médicos, desde comprados até fabricados.", 
+        img = "satchel_nav_remedies.png" 
+    },
+    foods = { types = { 0, 3 }, desc = "Armazena todos os tipos de provisões, desde compradas na loja até refeições cozidas.", img = "satchel_nav_provisions.png" },
+    weapons = { types = { 0, 5 }, desc = "Armazena todas as suas armas.", img = "weapon.png" },
+    ammo = { types = { 0, 6 }, desc = "Armazena todos os tipos de munição que você possui.", img = "ammo.png" },
+    tools = { types = { 0, 4 }, desc = "Armazena todos os tipos de ferramentas e materiais que você possui.", img = "satchel_nav_materials.png" },
+    animals = { types = { 0, 8 }, desc = "Armazena todos os tipos de itens que você coletou de animais.", img = "satchel_nav_animals.png" },
+    documents = { types = { 0, 7 }, desc = "Armazena todos os tipos de documentos que podem ser inspecionados ou utilizados.", img = "satchel_nav_documents.png" },
+    valuables = { types = { 0, 9 }, desc = "Armazena todos os itens de valor que podem ser vendidos por dinheiro.", img = "satchel_nav_valuables.png" },
+    horse = { types = { 0, 10 }, desc = "Exibe todos os itens que são usados para cavalos.", img = "satchel_nav_horse_items.png" },
+    herbs = { types = { 0, 11 }, desc = "Armazena todos os tipos de ervas que você coletou", img = "satchel_nav_herbs.png" },
+    tradable = { types = { 0, 11 }, desc = "Armazena todos os tipos de itens de troca", img = "satchel_nav_materials.png" },
+}
